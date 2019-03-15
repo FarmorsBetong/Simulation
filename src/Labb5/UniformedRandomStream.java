890b053e094814dce7bf2ea7759d95d41acecd43
VulcanianKri <jon.we@hotmail.se>
2019-03-15 17:20:10 +0100

    Jon Randomizers

--- src/Labb5/UniformedRandomStream.java
+++ src/Labb5/UniformedRandomStream.java
@@ -8,19 +8,33 @@ public class UniformedRandomStream {
 
     private Random rand;
     private double lower, width;
-
+/**
+ * 
+ * @param lower
+ * @param upper
+ * @param seed
+ */
     public UniformedRandomStream(double lower, double upper, long seed) {
-        rand = new Random(seed);
+    	// Sets random to use this seed.
+    	rand = new Random(seed);
         this.lower = lower;
         this.width = upper-lower;
     }
-
+/**
+ * 
+ * @param lower
+ * @param upper
+ */
     public UniformedRandomStream(double lower, double upper) {
+    	// Sets random run without seed.
         rand = new Random();
         this.lower = lower;
         this.width = upper-lower;
     }
-
+/**
+ * 
+ * @return A random double between lower and upper.
+ */
     public double next() {
         return lower+rand.nextDouble()*width;
     }
